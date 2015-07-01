19d148e1504650225aa4dfd4c6aa726f5028af38
ThatDerpyCode <cdriza2@gmail.com>
2015-06-30 19:58:57 -0700

    Create cc_list.lua

---
+++ CheaterCheck/lua/cc_list.lua
@@ -0,0 +1,55 @@
+CC.List = {
+	["STEAM_0:0:37511807"] = "cdriza (old main), C++ hacks, mpgh, huge skid, VAC bans",
+	["STEAM_0:0:83444632"] = "cdriza (old main), C++ hacks, mpgh, huge skid, VAC bans (Alt of STEAM_0:0:37511807)",
+	["STEAM_0:0:136834592"] = "cdriza (new main), C++ hacks, mpgh, huge skid, VAC bans (Alt of STEAM_0:0:37511807)",
+	
+	["STEAM_0:0:115343499"] = "kittix/wutarmelon, C++ hacks, mpgh, ratting, VAC ban",
+	
+	["STEAM_0:0:69773252"] = "Aria, C++ hacks, mpgh, VAC bans",
+	["STEAM_0:0:30559593"] = "Aria alt, C++ hacks, mpgh, VAC bans (Alt of STEAM_0:0:69773252)",
+	
+	["STEAM_0:0:100011237"] = "Razor, C++ hacks, mpgh, huge skid, VAC bans",
+	
+	["STEAM_0:0:71356345"] = "Kyouko/Fami, C++ hacks, mpgh, VAC bans",
+	
+	["STEAM_0:1:120338831"] = "Satori/Kieru, C++ hacks, mpgh, ratting, doxing, VAC bans",
+	
+	["STEAM_0:0:44427696"] = "Snip, C++ hacks, mpgh, huge skid, VAC bans",
+	
+	["STEAM_0:0:27489475"] = "Chef, C++ hacks, mpgh, huge skid, VAC bans",
+	
+	["STEAM_0:1:62445445"] = "Q2F2/Cave/Cave Johnson, old cheater",
+	
+	["STEAM_0:0:56486352"] = "Regan, C++ hacks, mpgh, huge skid, VAC bans",
+	
+	["STEAM_0:1:42964759"] = "NanoCat, C++ hacks, mpgh, VAC bans",
+	
+	["STEAM_0:0:42138604"] = "Liquid, C++ hacks, mpgh, VAC bans",
+	
+	["STEAM_0:0:101348488"] = "Leystryku, C++ hacks, mpgh, old cheater",
+	
+	["STEAM_0:1:98244245"] = "Korton/Anonymous, C++ hacks, triggercheats, mpgh, huge skid, VAC bans",
+	
+	["STEAM_0:0:68225583"] = "Herp Derpinstine, C++ hacks, mpgh, VAC bans",
+	
+	["STEAM_0:1:99708404"] = "TrollSploit, lua exploits, jay the exploiter wannabe, huge skid, VAC bans",
+	
+	["STEAM_0:0:33099483"] = "Polivilias/Poli, C++ hacks, mpgh, VAC bans",
+	
+	["STEAM_0:1:94707597"] = "Function, C++ hacks, triggercheats owner, bighack owner, mpgh, ratting, doxing, VAC bans",
+	["STEAM_0:0:5991293"] = "Function alt, C++ hacks, triggercheats owner, bighack owner, mpgh, ratting, doxing, VAC bans (Alt of STEAM_0:1:94707597)",
+	
+	["STEAM_0:0:60971854"] = "OJ, mpgh, VAC bans",
+	
+	["STEAM_0:1:41711091"] = "Skittles 2.0, C++ hacks, mpgh, VAC bans",
+	
+	["STEAM_0:0:63265223"] = "Kiwi, C++ hacks, mpgh, VAC bans",
+	
+	["STEAM_0:1:119450183"] = "st0rm, C++ hacks, mpgh, VAC bans",
+	
+	["STEAM_0:1:73012010"] = "Im Friendly, C++ hacks, mpgh, doxing, VAC bans",
+	
+	["STEAM_0:1:120437749"] = "Nub, C++ hacks, mpgh, huge skid, VAC bans",
+	
+	["STEAM_0:0:23976136"] = "SleepyCat, C++ hacks, mpgh, VAC bans",
+}
